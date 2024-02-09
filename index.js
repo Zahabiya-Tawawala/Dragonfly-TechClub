bae73d48d969c6b7f39c4c6a3f7881719532a5ce
Sujit Ghogare <116686235+itssg001@users.noreply.github.com>
2024-02-09 08:43:42 +0530

    pages added

---
+++ index.js
@@ -0,0 +1,9 @@
+// var sidemenu = documnent.getElmentById("sidemenu");
+// function openmenu(){
+//     sidemenu.style.right = "0"
+// }
+// openmenu()
+// function closemenu(){
+//     sidemenu.style.right = "-400px"
+// }
+// closemenu()
