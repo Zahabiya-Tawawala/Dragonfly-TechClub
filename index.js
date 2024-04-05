f10018fc5eacb147038d0f8b05ecf4e87b4a9dd1
Sujit Ghogare <116686235+itssg001@users.noreply.github.com>
2024-04-06 00:27:20 +0530

    project cleaned

--- index.js
+++
@@ -1,9 +0,0 @@
-// var sidemenu = documnent.getElmentById("sidemenu");
-// function openmenu(){
-//     sidemenu.style.right = "0"
-// }
-// openmenu()
-// function closemenu(){
-//     sidemenu.style.right = "-400px"
-// }
-// closemenu()
